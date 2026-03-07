4b0a44163781244c6febbf6a1a0bbe907ed4ce69
golamrabbi73 <golamrabbi39hr@gmail.com>
2026-03-07 14:48:38 +0600

    fix toggle button id name

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const allBtn = document.getElementById("all-toggle-btn");
-const openBtn = document.getElementById("open-toggle-btn");
-const closedBtn = document.getElementById("closed-toggle-btn");
+const allBtn = document.getElementById("all-tab-btn");
+const openBtn = document.getElementById("open-tab-btn");
+const closedBtn = document.getElementById("closed-tab-btn");
 
 // button toggle functionality
 const btnToggle = (id) =>{
-    if(id === "all-toggle-btn"){
+    if(id === "all-tab-btn"){
         allBtn.classList.add("btn-primary", "text-white")
         closedBtn.classList.remove("btn-primary", "text-white")
         openBtn.classList.remove("btn-primary", "text-white")
-    }else if(id === "open-toggle-btn"){
+    }else if(id === "open-tab-btn"){
         openBtn.classList.add("btn-primary", "text-white")
         allBtn.classList.remove("btn-primary", "text-white")
         closedBtn.classList.remove("btn-primary", "text-white")
         allBtn.classList.add("text-[#64748B]")
-    }else if(id === "closed-toggle-btn"){
+    }else if(id === "closed-tab-btn"){
         closedBtn.classList.add("btn-primary", "text-white")
         allBtn.classList.remove("btn-primary", "text-white")
         openBtn.classList.remove("btn-primary", "text-white")
         allBtn.classList.add("text-[#64748B]")
     }
 }
+
+
